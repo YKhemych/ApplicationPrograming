bf6c426a1a7ddaf987da7c916134b7aa7b0f11e9
YKhemych <yosyp0108khemych@gmail.com>
2018-12-04 10:49:44 +0200

    lab 6 fixed

--- Lab6/task2.py
+++ Lab6/task2.py
@@ -8,8 +8,7 @@ def task2_1():
 
 def task2_2():
     data = pd.read_csv('NYC_Jobs.csv')
-    pd.options.display.max_columns = 3
-    print(data[['Agency', 'Business Title', 'Work Location 1']].to_string(index=False))
+    print(data[['Agency', 'Business Title', 'Work Location 1']])
 
 
 if __name__=="__main__":
--- Lab6/task3.py
+++ Lab6/task3.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('NYC_Jobs.csv')
 names = ['Agency', '# Of Positions']
-print(df[names])
+# print(df[names])
 df['Agency'].value_counts(sort = True).plot.bar()
 plt.show()
